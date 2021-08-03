d56b71965daeaf62e4655095e4838e8c5d987c52
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2021-08-03 13:04:33 +0200

    Correct path in __init__.py

--- clang-format/__init__.py
+++ clang-format/__init__.py
@@ -4,5 +4,5 @@ import sys
 
 def clang_format():
     name = "clang-format"
-    executable = os.path.join(os.path.split(__file__)[0], "bin", name)
+    executable = os.path.join(os.path.split(__file__)[0], "data", "bin", name)
     return os.execv(executable, [name] + sys.argv[1:])
