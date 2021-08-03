3cace0c5afc288c5635b6455de7e22bfbe6e3cd6
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2021-08-03 17:22:00 +0200

    Add forgotten entrypoint script

--- setup.py
+++ setup.py
@@ -3,11 +3,16 @@ from skbuild import setup
 
 setup(
     name="clang-format",
-    version="0.0.2",
+    version="0.0.3",
     author="Dominic Kempf",
     author_email="ssc@iwr.uni-heidelberg.de",
     packages=["clang-format"],
     zip_safe=False,
+    entrypoints={
+        "clang-format": [
+            "clang-format=clang-format:clang-format"
+        ]
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
