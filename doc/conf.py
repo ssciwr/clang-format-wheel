e2227b88931aee816dd2621d3c8dd51cb32d0edc
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2021-08-04 13:49:21 +0200

    Add a sphinx documentation

---
+++ doc/conf.py
@@ -0,0 +1,38 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'clang-format-wheel'
+copyright = '2021, Scientific Software Center, Heidelberg University'
+author = 'Dominic Kempf'
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "m2r2",
+    "sphinx_rtd_theme",
+]
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
---
+++ doc/index.rst
@@ -0,0 +1,6 @@
+.. mdinclude:: ../README.md
+
+How to add a release
+--------------------
+
+These are the steps necessary to add a new release to PyPI:
