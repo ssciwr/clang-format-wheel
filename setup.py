c918a8cb9adf36cead1cbb0a41a0a969933cb922
Ryan Mast <mast.ryan@gmail.com>
2021-08-13 21:29:50 -1000

    Use py2.py3 for bdist platform tags

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
+
+from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 from skbuild import setup
 
 import re
 
+class genericpy_bdist_wheel(_bdist_wheel):
+    def finalize_options(self):
+        _bdist_wheel.finalize_options(self)
+        self.root_is_pure = False
+
+    def get_tag(self):
+        python, abi, plat = _bdist_wheel.get_tag(self)
+        python, abi = "py2.py3", "none"
+        return python, abi, plat
 
 # Read the clang-format version from the "single source of truth"
 def get_version():
@@ -17,10 +28,11 @@ def get_version():
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-
+cmdclass = {"bdist_wheel": genericpy_bdist_wheel}
 setup(
     name="clang-format",
     version="0.0.7",
+    cmdclass=cmdclass,
     author="Dominic Kempf",
     author_email="ssc@iwr.uni-heidelberg.de",
     packages=["clang_format"],
