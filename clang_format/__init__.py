57a4e94b7c051b20efbf31230b9d7e4de413862d
fgrie <62984779+fgrie@users.noreply.github.com>
2022-04-25 17:23:40 +0200

    Fix script execution on Windows
    As MS Windows is not able to run Python scripts directly by name, we have to call the interpreter and pass the script as parameter instead of calling it directly.
    This has to be done for `clang-format-diff.py` as well as `git-clang-format`, as these two are Python script.
    The call of `clang-format` must be left unchanged, as this is a binary executable which has to be called directly.

--- clang_format/__init__.py
+++ clang_format/__init__.py
@@ -3,9 +3,18 @@ import subprocess
 import sys
 
 
+def _get_executable(name):
+    return os.path.join(os.path.dirname(__file__), "data", "bin", name)
+
 def _run(name):
-    executable = os.path.join(os.path.dirname(__file__), "data", "bin", name)
+    executable = _get_executable(name)
     return subprocess.call([executable] + sys.argv[1:])
+
+def _run_python(name):
+    executable = _get_executable(name)
+    # as MS Windows is not able to run Python scripts directly by name,
+    # we have to call the interpreter and pass the script as parameter
+    return subprocess.call([sys.executable, executable] + sys.argv[1:])
 
 
 def clang_format():
@@ -13,8 +22,8 @@ def clang_format():
 
 
 def clang_format_diff():
-    raise SystemExit(_run("clang-format-diff.py"))
+    raise SystemExit(_run_python("clang-format-diff.py"))
 
 
 def git_clang_format():
-    raise SystemExit(_run("git-clang-format"))
+    raise SystemExit(_run_python("git-clang-format"))
