b51f7f3566648fda90393ae051c062604899c70a
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2022-05-05 09:26:14 +0200

    Add test that the git-clang-format tool is executable

--- test/test_clang_format.py
+++ test/test_clang_format.py
@@ -15,7 +15,15 @@ def test_clang_format(testcase):
     with tempfile.TemporaryDirectory() as tmp:
         outname = os.path.join(tmp, "formatted")
         with open(outname, "w") as out:
-            subprocess.run(["clang-format", test_input], stdout=out)
+            subprocess.run(["clang-format", test_input], stdout=out, check=True)
 
         # Check that the content is equal
         assert filecmp.cmp(outname, test_output)
+
+
+def test_git_clang_format():
+    # Test whether the git-clang-format tool is properly executable
+    # on an empty git repository.
+    with tempfile.TemporaryDirectory() as tmp:
+        subprocess.run("git init".split())
+        subprocess.run("git clang-format".split(), check=True)
